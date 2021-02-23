256f323873b1790fdcdd51f9870c2ab8811d9c91
Brian Meier <brianmeier@amalgama.co>
2021-02-22 21:31:33 -0300

    Add feature to copy default commit message

---
+++ src/Components/CommitIcon/CommitIcon.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import CommitIconPNG from '../../icons/commit.png';
+
+const CommitIcon = () => (
+	<img src={CommitIconPNG} alt="commit-icon" />
+);
+
+export default CommitIcon;
